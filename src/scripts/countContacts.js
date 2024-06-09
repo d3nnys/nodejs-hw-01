62f8aa1a5a686d43aea5e5e68bed0f48fcb5fedd
Denny <d3n1sf4z49@gmail.com>
2024-06-09 22:39:55 +0200

    step fifth

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,15 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
-import { createFakeContact } from '../utils/createFakeContact.js';
 import fs from 'node:fs';
 
 export const countContacts = async () => {
   try {
-    const data = fs.readFile(PATH_DB, 'utf-8');
+    const data = fs.readFileSync(PATH_DB, 'utf-8');
     const contacts = JSON.parse(data);
-
-    contacts.push(createFakeContact);
-
-    fs.writeFile(PATH_DB, JSON.stringify(contacts), 'utf-8');
+    return contacts.length;
   } catch (error) {
     console.log(error.message);
   }
